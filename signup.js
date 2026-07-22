b1c77c42f4427014c7a68610fb39f29345e53df0
joshirajan704-boop <joshirajan704@gmail.com>
2026-07-22 22:18:21 +0530

    Add signup functionality using Firebase authentication
    Implement user signup functionality with email and password.

--- signup.js
+++ signup.js
@@ -1,1 +1,25 @@
+import {
+  auth,
+  createUserWithEmailAndPassword
+} from "./firebase.js";
 
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const signupBtn = document.getElementById("signupBtn");
+
+signupBtn.addEventListener("click", async () => {
+  try {
+    await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    alert("Account Created Successfully!");
+
+    window.location.href = "login.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+});
